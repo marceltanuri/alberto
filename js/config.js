5c7f76327bd9979123b039b8b0fa75ba22bc9859
Marcel Tanuri <marcel.tanuri@gmail.com>
2022-06-07 10:03:39 +0100

    initial commit

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const _spreadsheetId = '1C04zbcV1UlxmS5G-1QWUT9OslSbJWhhCz7gwOZzvMQU'
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '1058634479009-tenrbcqlhuae3hqnrgnlp11tjotap5ao.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyAQtFCqLuNKPSA5_8lHYakulT_qMDn6Mfw';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
--- js/readSpreadsheet.js
+++ js/readSpreadsheet.js
@@ -3,17 +3,6 @@
 /* exported handleAuthClick */
 /* exported handleSignoutClick */
 
-// TODO(developer): Set to client ID and API key from the Developer Console
-const CLIENT_ID = '1058634479009-tenrbcqlhuae3hqnrgnlp11tjotap5ao.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyAQtFCqLuNKPSA5_8lHYakulT_qMDn6Mfw';
-
-// Discovery doc URL for APIs used by the quickstart
-const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
-
 let tokenClient;
 let gapiInited = false;
 let gisInited = false;
@@ -112,8 +101,6 @@ async function readSpreadsheet() {
         "data": [],
         "incomings": []
     }
-
-    let _spreadsheetId = '1C04zbcV1UlxmS5G-1QWUT9OslSbJWhhCz7gwOZzvMQU'
 
     try {
         let response = await gapi.client.sheets.spreadsheets.values.get({
