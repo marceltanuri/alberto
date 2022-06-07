0f1bf0f3f49e16efeea231fa8523bea464fb4d39
Marcel Tanuri <marcel.tanuri@gmail.com>
2022-06-07 10:50:26 +0100

    initial commit

--- js/readSpreadsheet.js
+++ js/readSpreadsheet.js
@@ -84,7 +84,7 @@ function handleSignoutClick() {
     if (token !== null) {
         google.accounts.oauth2.revoke(token.access_token);
         gapi.client.setToken('');
-        document.getElementById('content').innerText = '';
+        document.querySelector(".container-report").style.display = 'none'
         document.getElementById('authorize_button').innerText = 'Authorize';
         document.getElementById('password').style.visibility = 'visible';
         document.getElementById('signout_button').style.visibility = 'hidden';
